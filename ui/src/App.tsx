297906ba833e892a1c9b4575d6200e3bd030881d
Sparsh Srivastava <ss6381@columbia.edu>
2023-03-10 19:35:53 -0800

    connected frontend and backend

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Button from "./components/Button";
+// import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Glad to have you here at SparshApp</h1>
-        <Button />
+        {/* <Button /> */}
+        <Header />
       </header>
     </div>
   );
---
+++ ui/src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { getUsers } from '../../services/api';
+
+const Header = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getUsers();
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <p>Here is some data fetched from the API:</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Header;
